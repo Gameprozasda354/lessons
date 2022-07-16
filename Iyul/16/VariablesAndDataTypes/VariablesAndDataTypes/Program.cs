ee61ba159bb4164e89712de969aa621d525d6313
Orkhan Farajov <orkhankhf@gmail.com>
2022-07-16 15:52:21 +0300

    Variables and data types

---
+++ Iyul/16/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        public static string Country { get; set; } = "Azerbaijan";
+
+        static void Main(string[] args)
+        {
+            #region Local vs Global Variables
+            Console.WriteLine(Country);
+
+            int myLocalVariable = 5;
+            Console.WriteLine(myLocalVariable);
+
+            int globalVar = 0;
+
+            {
+                int localVar1 = 555;
+                globalVar += localVar1;
+            }
+
+            {
+                int localVar2 = 45;
+                globalVar += localVar2;
+            }
+
+            {
+                int localVar3 = 50;
+                globalVar += localVar3;
+            }
+            #endregion
+
+            Console.ReadLine();
+        }
+
+        public static void ShowCountry()
+        {
+            Console.WriteLine(Country);
+        }
+    }
+}
