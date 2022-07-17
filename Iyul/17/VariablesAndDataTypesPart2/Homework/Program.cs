bd9735cd9a2841cac4ac31805ca28a86e84b29ae
Orkhan Farajov <orkhankhf@gmail.com>
2022-07-17 13:18:12 +0300

    Homework of 17

---
+++ Iyul/17/VariablesAndDataTypesPart2/Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                  Kiçik bir anket app'ı hazırlayacaqsız. İlk öncə istifadəçiyə Xoş Gəldiniz! mesajı göstərin. Növbəti sətirdə adını soruşun. Adını daxil
+                  etdikdən sonra ona necə müraciət edəcəyini soruşun. Məsələn adını Orxan, müraciət qaydasını bəy olaraq yazdısa növbəti suallar belə
+                  olacaq:
+                  1. Orxan bəy neçə yaşınız var?
+                  2. Orxan bəy hansı sahədə işləyirsiniz?
+                  3. Orxan bəy ən son nə zaman kitab oxumusunuz?
+                  4. Orxan bəy sevdiyiniz rəng hansıdır?
+                  5. Orxan bəy avtomobil idarə edə bilirsiniz?
+                  6. Orxan bəy gündə neçə saat yatırsınız?
+                  7. Orxan bəy TV kanallarını izləyirsiniz?
+                  
+                  Əgər istifadəçi müraciət qaydası olaraq bəy/xanım daxil etməsə o zaman birbaşa Orxan neçə yaşınız var? soruşsun.
+                  Axırıncı sualdan sonra "Anketə qatıldığınız üçün təşəkkür edirik!" yazsın.
+             */
+        }
+    }
+}
