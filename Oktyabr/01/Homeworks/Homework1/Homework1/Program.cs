b674f7daa5c646dea4bf8f65789d340d1021e221
Omer Isgenderli <omerisgzoom@gmail.com>
2022-10-06 21:01:06 +0400

    ldhsh

---
+++ Oktyabr/01/Homeworks/Homework1/Homework1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student();
+
+            student.Name = "Orkhan"; 
+            student.SurName = "Farajov";
+
+            Console.WriteLine(student.Email);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Oktyabr/01/Homeworks/Homework1/Homework1/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    internal class Student
+    {
+
+        private string name;
+        private string surName;
+        private string email;
+
+
+        public string Name
+        {
+            set
+            {
+                if(surName == null)
+                {
+                    name = value;
+                    surName = null;
+                    email = null;
+                }
+
+                else
+                {
+                    name = value;
+                    email = value.ToLower() + "." + surName.ToLower() + "@gmail.com";
+                }
+
+            }
+
+            get
+            {
+                return name;
+            }
+        }
+
+        public string SurName
+        {
+            set
+            {
+                if (name == null)
+                {
+                    surName = value;
+                    name = null;
+                    email = null;
+                }
+
+                else
+                {
+                    surName = value;
+                    email = name.ToLower() + "." + value.ToLower() + "@gmail.com";
+                }
+            }
+
+            get
+            {
+                return surName;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+        }
+
+    }
+}
