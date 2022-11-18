fd72d072b3e53aa1853bfc8091d733a8dbfef089
Gameprozasda354 <omerisgzoom@gmail.com>
2022-11-18 13:53:45 +0400

    dc

---
+++ Noyabr/05/test/test/Base.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Base
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public string Job { get; set; }
+
+        public virtual void ShowAllInfo()
+        {
+            Console.WriteLine("Name:" + Name);
+            Console.WriteLine("Surname:" + SurName);
+            Console.WriteLine("Age:" + Age);
+            Console.WriteLine("Job:" + Job);
+        }
+    }
+}
---
+++ Noyabr/05/test/test/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Person : Base
+    {
+        public override void ShowAllInfo()
+        {
+            Console.WriteLine("Name:" + Name);
+            Console.WriteLine("Surname:" + SurName);
+            Console.WriteLine("This man is Happy :)");
+        }
+    }
+}
