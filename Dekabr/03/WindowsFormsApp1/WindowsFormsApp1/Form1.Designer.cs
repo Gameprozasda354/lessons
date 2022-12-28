8244154dbf361c44edd04405c4b54724623a6209
Gameprozasda354 <omerisgzoom@gmail.com>
2022-12-28 15:50:18 +0400

    added

---
+++ Dekabr/03/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.textSenderBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gondermek istediyiniz \r\nsozu daxil edin:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(34, 56);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(148, 20);
+            this.textBox.TabIndex = 2;
+            this.textBox.Text = "Mesaj daxil edin...";
+            this.textBox.Enter += new System.EventHandler(this.textBox_Enter);
+            // 
+            // textSenderBtn
+            // 
+            this.textSenderBtn.Location = new System.Drawing.Point(34, 82);
+            this.textSenderBtn.Name = "textSenderBtn";
+            this.textSenderBtn.Size = new System.Drawing.Size(148, 35);
+            this.textSenderBtn.TabIndex = 1;
+            this.textSenderBtn.Text = "Mesaji gonder.";
+            this.textSenderBtn.UseVisualStyleBackColor = true;
+            this.textSenderBtn.Click += new System.EventHandler(this.textSenderBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textSenderBtn);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TextSenderApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button textSenderBtn;
+    }
+}
+
---
+++ Dekabr/03/WindowsFormsApp1/WindowsFormsApp1/WindowsForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class WindowsForm : Form
+    {
+        public WindowsForm(string _sendedText)
+        {
+            InitializeComponent();
+            sendedText.Text = _sendedText;
+        }
+    }
+}
