858f1141a8895f9f48b2e54106c97a537dd5075f
Gameprozasda354 <omerisgzoom@gmail.com>
2023-02-02 17:08:08 +0400

    adde

---
+++ 2023/Yanvar/26/Homework/Homework/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Car
+    {
+        public string Name { get; set; } 
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Country { get; set; }
+        public int Year { get; set; }
+        public int Km { get; set; }
+        public string Color { get; set; }
+        public int NumberOfSeats { get; set; }
+        public bool IsNew { get; set; }
+        public double Engine { get; set; }
+        public string GearBox { get; set; }
+        public DateTime UptadeDate { get; set; }
+        public DateTime PutDate { get; set; }
+    }
+}
