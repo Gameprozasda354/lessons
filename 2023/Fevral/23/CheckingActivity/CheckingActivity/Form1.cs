a2f2db5a2c90f37b3a4272eb20fc82348d5e968a
Orkhan Farajov <orkhankhf@gmail.com>
2023-02-23 19:40:14 +0400

    Refactoring codes

---
+++ 2023/Fevral/23/CheckingActivity/CheckingActivity/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckingActivity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int cursorTime = 0;
+
+            var xPos = Cursor.Position.X;
+            var yPos = Cursor.Position.Y;
+
+            if(xPos == Cursor.Position.X && yPos == Cursor.Position.Y)
+            {
+                cursorTime++;
+            }
+            else
+            {
+                cursorTime = 0;
+            }
+
+            if (cursorTime > 5)
+                MessageBox.Show("Salam");
+            
+        }
+    }
+}
