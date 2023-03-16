154a3301ef01dbd2bd76e5e6fcf406e267be2b1f
Orkhan Farajov <orkhankhf@gmail.com>
2023-03-16 21:45:25 +0400

    Homeworks added

---
+++ 2023/Mart/16/MsSQL - Start/Homework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Homework 1:
+             Dərsdə restore etdiyiniz AdventureWorks2017 database'ini silin və yenidən restore edin.
+
+             Homework 2:
+             Aşağıdakı mövzuları araşdırın:
+             Sql Server Management Studio'nun nə olduğu haqqında araşdırma edin.
+             Database nədir?
+             Table nədir?
+             Column nədir?
+             Row nədir?
+
+             Homework 3:
+             AdventureWorks2017 database'ində aşağıdakı məlumatları almaq üçün lazım olan select sorğularını yazın.
+             Store adlı table'dan bütün column'ları select edin.
+             Product adlı table'dan bütün column'ları select edin.
+             ProductCategory adlı table'dan sadəcə ProductCategoryID və Name column'larını select edin.
+             AddressType adlı table'dan sadəcə AddressTypeID və Name column'larını select edin.
+             Document adlı table'dan sadəcə Title, FileName, ChangeNumber və Status column'larını select edin.
+
+             Araşdırıb və ya düşünüb yaza biləcəyiniz bonus sorğu (yaza bilməsəniz eybi yox):
+             Store table'ından bütün məlumatları * ilə çəkin. Sadəcə ilk column SalesPersonID olsun.
+             Bu sorğuda SalesPersonID iki dəfə çıxsa da olar eybi yox.
+             */
+        }
+    }
+}
