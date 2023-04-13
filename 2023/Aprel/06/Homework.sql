8263c5034707713873b4c23c2b58bedef4e12cd2
Gameprozasda354 <omerisgzoom@gmail.com>
2023-04-13 19:07:08 +0400

    Homework

--- 2023/Aprel/06/Homework.sql
+++ 2023/Aprel/06/Homework.sql
@@ -3,8 +3,47 @@ Homework 1:
 Ev tapşırığı olaraq custom function'lar yaratmağı öyrənib, kiçik bir calculator app'ı yazacaqsınız.
 C#'da etdiyiniz calculator app'lar kimi hazırlaya bilərsiniz. Əsas məsələ custom function'larla
 işləməkdir.
+*/
 
+CREATE FUNCTION Calculator(@firstNumber int = 0, @secondNumber int = 0, @operation nvarchar(10))
+RETURNS int
+AS
+BEGIN
+	DECLARE @answer int
+    if (@operation = 'Addition')
+		set @answer = (@firstNumber + @secondNumber)
+	if (@operation = 'Substract')
+		set @answer = (@firstNumber - @secondNumber)
+	if (@operation = 'Multiple')
+		set @answer = (@firstNumber * @secondNumber)
+	if (@operation = 'Division')
+		set @answer = (@firstNumber / @secondNumber)
+
+	return @answer
+END
+GO
+
+/*
 Homework 2:
 C#'da bildiyiniz bütün data tiplərin SQL'dəki qarşılığını tapın. Bir table yaradıb, C#'da bildiyiniz bütün data tiplərinin SQL'də
 olan qarşılığından istifadə edərək praktika edin. İnsertlərini də yazın mütləq.
 */
+
+create table DataTypes
+(
+	Bool nvarchar(20),
+	Int16 nvarchar(20),
+	Int32 nvarchar(20),
+	Int64 nvarchar(20),
+	String nvarchar(20),
+	Decimal1 nvarchar(20),
+	Float1 nvarchar(20),
+	Char1 nvarchar(20),
+	DateTime1 nvarchar(20)
+
+)
+
+insert into DataTypes(Bool, Int16, Int32, Int64, String, Decimal1, Float1, Char1, DateTime1)
+Values ('bit', 'smallint', 'int', 'bigint', 'nvarchar', 'decimal', 'float', 'char', 'datetime')
+
+
---
+++ 2023/Aprel/06/Homework/Homework/Homework/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDocument = new XDocument(
+                new XDeclaration("1.0", "UTF-8", "yes"),
+                new XComment("Tələbə məlumatları"),
+                new XElement("Students",
+                    new XComment("Tələbə 1"),
+                    new XElement("Student", 
+                        new XAttribute("Id", "1"),
+                        new XElement("Name", "Ismayıl"),
+                        new XElement("Surname", "Məmmədli"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "4.7.2000"),
+                        new XElement("Degree", "Bachelor's"),
+                        new XElement("Contact", 
+                            new XElement("Adress", "Azerbaijan, Baku, Yasamal"),
+                            new XElement("Phone", "050-222-55-33")
+                        )
+                    
+                    ),
+                    new XComment("Tələbə 2"),
+                    new XElement("Student",
+                        new XAttribute("Id", "2"),
+                        new XElement("Name", "Əli"),
+                        new XElement("Surname", "İsmayılzadə"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "5.20.2004"),
+                        new XElement("Degree", "Associate"),
+                        new XElement("Contact",
+                            new XElement("Adress", "Azerbaijan, Sumqayıt, 17-ci mkr"),
+                            new XElement("Phone", "070-879-57-46")
+                        )
+
+                    ),
+                    new XComment("Tələbə 3"),
+                    new XElement("Student",
+                        new XAttribute("Id", "3"),
+                        new XElement("Name", "Rövşən"),
+                        new XElement("Surname", "Məmmədli"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "8.23.2010"),
+                        new XElement("Degree", "Doctoral"),
+                        new XElement("Contact",
+                            new XElement("Adress", "Azerbaijan, Sumqayıt, Saray"),
+                            new XElement("Phone", "055-247-90-07")
+                        )
+
+                    ),
+                    new XComment("Tələbə 4"),
+                    new XElement("Student",
+                        new XAttribute("Id", "4"),
+                        new XElement("Name", "Nurlan"),
+                        new XElement("Surname", "Əhmədli"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "2.3.2005"),
+                        new XElement("Degree", "Master's"),
+                        new XElement("Contact",
+                            new XElement("Adress", "Azerbaijan, Baku, Nizami"),
+                            new XElement("Phone", "077-292-50-24")
+                        )
+
+                    ),
+                    new XComment("Tələbə 5"),
+                    new XElement("Student",
+                        new XAttribute("Id", "5"),
+                        new XElement("Name", "Aqşin"),
+                        new XElement("Surname", "Vəlizadə"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "1.17.1997"),
+                        new XElement("Degree", "Associate"),
+                        new XElement("Contact",
+                            new XElement("Adress", "Azerbaijan, Baku, Gənclik"),
+                            new XElement("Phone", "050-977-70-73")
+                        )
+
+                    ),
+                    new XComment("Tələbə 6"),
+                    new XElement("Student",
+                        new XAttribute("Id", "6"),
+                        new XElement("Name", "Emil"),
+                        new XElement("Surname", "Məlikov"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "7.22.2014"),
+                        new XElement("Degree", "Master's"),
+                        new XElement("Contact",
+                            new XElement("Adress", "Azerbaijan, Baku, Koroğlu"),
+                            new XElement("Phone", "050-211-08-96")
+                        )
+
+                    ),
+                    new XComment("Tələbə 7"),
+                    new XElement("Student",
+                        new XAttribute("Id", "7"),
+                        new XElement("Name", "Əhməd"),
+                        new XElement("Surname", "Hüseynzadə"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "11.10.2011"),
+                        new XElement("Degree", "Doctoral"),
+                        new XElement("Contact",
+                            new XElement("Adress", "Azerbaijan, Baku, Heydər Əliyev prospekti"),
+                            new XElement("Phone", "055-798-00-05")
+                        )
+
+                    ),
+                    new XComment("Tələbə 8"),
+                    new XElement("Student",
+                        new XAttribute("Id", "8"),
+                        new XElement("Name", "Ələsgər"),
+                        new XElement("Surname", "Əliyev"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "8.30.1980"),
+                        new XElement("Degree", "Bachelor's"),
+                        new XElement("Contact",
+                            new XElement("Adress", "Azerbaijan, Sumqayıt, 9-cu mkr"),
+                            new XElement("Phone", "070-229-00-00")
+                        )
+
+                    ),
+                    new XComment("Tələbə 9"),
+                    new XElement("Student",
+                        new XAttribute("Id", "9"),
+                        new XElement("Name", "Elşən"),
+                        new XElement("Surname", "Həmidov"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "9.1.2000"),
+                        new XElement("Degree", "Doctoral"),
+                        new XElement("Contact",
+                            new XElement("Adress", "Azerbaijan, Baku, Yasamal"),
+                            new XElement("Phone", "055-220-50-30")
+                        )
+
+                    ),
+                    new XComment("Tələbə 10"),
+                    new XElement("Student",
+                        new XAttribute("Id", "10"),
+                        new XElement("Name", "Əsgər"),
+                        new XElement("Surname", "Abdullayev"),
+                        new XElement("Gender", "Male"),
+                        new XElement("Birthdate", "2.26.2015"),
+                        new XElement("Degree", "Associate"),
+                        new XElement("Contact",
+                            new XElement("Adress", "Azerbaijan, Sumqayıt, 11-ci mkr"),
+                            new XElement("Phone", "070-297-00-00")
+                        )
+
+                    )
+
+                )
+            );
+
+            xDocument.Save(@"C:\\XML\\Students.xml");
+        }
+    }
+}
