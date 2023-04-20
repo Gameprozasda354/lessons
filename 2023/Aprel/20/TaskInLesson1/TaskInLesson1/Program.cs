230522ba0c7bfe249ebfe405801f4ec6cb04e594
Gameprozasda354 <omerisgzoom@gmail.com>
2023-04-20 21:24:38 +0400

    TaskInLesson

---
+++ 2023/Aprel/20/TaskInLesson1/TaskInLesson1/Program.cs
@@ -0,0 +1,24 @@
+﻿using MathOperations;
+using MathOperations.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskInLesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathoperation = new MathOperation();
+
+            decimal a = 70;
+            decimal b = 40;
+
+            decimal result = mathoperation.Calculate(a, b, MathOperationTypeEnum.Addition);
+
+        }
+    }
+}
