56a2eee69c748ef7bc1fabd9871a886b2ea6ec2e
Gameprozasda354 <omerisgzoom@gmail.com>
2023-05-02 20:49:25 +0400

    EduSys

---
+++ 2023/Aprel/27/OmersProject/EduSys/EduSys.Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EduSys.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduSys.Repository.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn(1, 1);
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+
+            builder.ToTable("Categories");
+        }
+    }
+}
---
+++ 2023/Aprel/27/OmersProject/EduSys/EduSys.Repository/Configurations/ProductFeatureConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using EduSys.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduSys.Repository.Configurations
+{
+    public class ProductFeatureConfiguration : IEntityTypeConfiguration<ProductFeature>
+    {
+        public void Configure(EntityTypeBuilder<ProductFeature> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.HasOne(x => x.Product).WithOne(x => x.ProductFeature).HasForeignKey<ProductFeature>(x => x.ProductId);
+        }
+    }
+}
